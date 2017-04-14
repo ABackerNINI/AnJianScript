26b663d3ced530db6c95bf24853ea41d9870867a
ABackerNINI <abacker.nini@qq.com>
2017-04-14 14:16:02 +0800

    添加刷组队，添加cp_to_here(all)

---
+++ copy_to_here.bat
@@ -0,0 +1,3 @@
+@copy "C:\Program Files (x86)\按键精灵\按键精灵手机助手\Script\刷结界突破*.mq" .\刷结界突破\
+@copy "C:\Program Files (x86)\按键精灵\按键精灵手机助手\Script\刷组队*.mq" .\刷组队\
+@pause
---
+++ 刷组队/copy_to_here.bat
@@ -0,0 +1,3 @@
+copy "C:\Program Files (x86)\按键精灵\按键精灵手机助手\Script\刷组队*.mq" .
+
+::pause
