083ad4c00ea645138ce3096708127face7f0a19c
ABackerNINI <abacker.nini@qq.com>
2017-04-16 01:23:12 +0800

    add 副本_单人

--- copy_to_here.bat
+++ copy_to_here.bat
@@ -1,4 +1,5 @@
 @copy "C:\Program Files (x86)\按键精灵\按键精灵手机助手\Script\突破_组队*.mq" .\突破_组队\
 @copy "C:\Program Files (x86)\按键精灵\按键精灵手机助手\Script\组队_组队*.mq" .\组队_组队\
 @copy "C:\Program Files (x86)\按键精灵\按键精灵手机助手\Script\副本_组队*.mq" .\副本_组队\
+@copy "C:\Program Files (x86)\按键精灵\按键精灵手机助手\Script\副本_单人*.mq" .\副本_单人\
 @pause
